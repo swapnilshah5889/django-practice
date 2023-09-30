55cb6d239f16259ed491e739521e116f89d93cc6
Swapnil Shah <swapnilshah5889@gmail.com>
2023-09-29 20:26:57 -0400

    Added bakedgoods and cars model

--- example_app/admin.py
+++ example_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from example_app.models import BakedGood
+from example_app.models import Car
 # Register your models here.
+admin.site.register(BakedGood)
+admin.site.register(Car)
--- example_app/models.py
+++ example_app/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class BakedGood(models.Model): 
+    name = models.CharField(max_length=64)
+    desc = models.CharField(max_length=256)
+    good_type = models.CharField(
+        max_length=2,
+        choices=[
+            ('BG',"Bagel"),
+            ('BR',"Bread"),
+            ('CO',"Cookie"),
+            ('CA',"Cake"),
+            ('PR',"Pretzel")
+        ]
+    )
+    price = models.DecimalField(max_digits=6, decimal_places=2) 
+    recipe = models.TextField()
+
+class Car(models.Model):
+    CAR_BRANDS = [  
+        ('Toyota', 'Toyota'),
+        ('Honda', 'Honda'),
+        ('Ford', 'Ford'),
+        ('Chevrolet', 'Chevrolet'),
+        ('BMW', 'BMW'),
+        ('Mercedes', 'Mercedes'),
+        ('Acura', 'Acura'),
+        ('Nissan', 'Nissan'),
+    ]
+
+    make = models.CharField(max_length=50, choices=CAR_BRANDS)
+    model = models.CharField(max_length=50)
+    year = models.PositiveIntegerField(max_length=4, validators=[MinLengthValidator(4)])
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=20, blank=True, null=True)
+    mileage = models.PositiveIntegerField()
+    description = models.TextField(blank=True, null=True)
